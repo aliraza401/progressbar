8d9318a31bdd73c1d118323f6c0917de997d1c83
Ghost <alirazamunir401@gmail.com>
2023-05-22 16:53:38 +0500

    line overflow

--- src/ProgressBar/ProgressBar.styled.ts
+++ src/ProgressBar/ProgressBar.styled.ts
@@ -1,24 +1,25 @@
 import styled from "styled-components";
 
+export const ProgressBarMarker = styled.div<{ position: number }>`
+  position: absolute;
+  bottom: -10%; // Adjust the top position to prevent overflow at the top: ;
+  left: ${(props) => `${props.position}%`};
+  height: 110%;
+  border-left: 2px solid darkgray;
+`;
+
 export const ProgressBarWrapper = styled.div`
   width: 100%;
   height: 44px;
-  background: lightgray;
+  background: gray;
   position: relative;
+  overflow: visible; 
 `;
 
 export const ProgressBarItem = styled.div<{ completed: number }>`
   height: 100%;
   width: ${(props) => `${props.completed}%`};
   background: lightblue;
-`;
-
-export const ProgressBarMarker = styled.div<{ position: number }>`
-  position: absolute;
-  top: 0;
-  left: ${(props) => `${props.position}%`};
-  height: 100%;
-  border-left: 0.15rem solid darkgray;
 `;
 
 export const TaskCounter = styled.div`
@@ -30,7 +31,7 @@ export const TaskCounter = styled.div`
 
 export const Label = styled.div<{ position: number }>`
   position: absolute;
-  bottom: -28px;
+  bottom: -32px;
   left: ${(props) => `${props.position}%`};
   text-align: center;
 `;
